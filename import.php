44a1746630de975d98f2da53ae8e38e866b50a79
FallenGravity <ethanvb55@gmail.com>
2019-05-17 20:38:50 +0200

    Major Style Updates

--- import.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $_POST;
-$email = isset( $_POST['email'] ) ? $_POST['email'] : '';
-if ( ! empty ( $email ) ) {
-	$file    = 'email.csv';
-	$current = file_get_contents( $file );
-	$current .= $email . ';' . PHP_EOL;
-	file_put_contents( $file, $current );
-}
